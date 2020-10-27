c3d3c35abe16b96981950964f0de9231e6e1b31e
Mikhail M <47109585+MikhailMasny@users.noreply.github.com>
2020-10-27 03:21:24 +0300

    feat: add formik examples

---
+++ react-formik/src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BasicComponent from "../components/BasicComponent";
+import SignUpForm from "../components/SignUpForm";
+
+function App() {
+  return (
+    <div>
+      <BasicComponent />
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default App;
